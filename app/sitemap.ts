b32352b7bf23ac12e6f553bab51629b308eaa62c
Lukaska98 <kaminskaslukas98@gmail.com>
2026-06-15 01:03:19 +0300

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,13 @@
 export default function sitemap() {
   return [
-    {
-      url: "https://skiferhus.no",
-    },
-    {
-      url: "https://skiferhus.no/galleri/skifertak",
-    },
-    {
-      url: "https://skiferhus.no/galleri/fasader",
-    },
-    {
-      url: "https://skiferhus.no/galleri/uteomrader",
-    },
+    { url: "https://skiferhus.no" },
+
+    { url: "https://skiferhus.no/produkter" },
+    { url: "https://skiferhus.no/tjenester" },
+    { url: "https://skiferhus.no/kontakt" },
+
+    { url: "https://skiferhus.no/galleri/skifertak" },
+    { url: "https://skiferhus.no/galleri/fasader" },
+    { url: "https://skiferhus.no/galleri/uteomrader" },
   ];
 }
